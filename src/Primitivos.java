e5405d66963e7a56ca71ad7b349f83efbb02fd1c
ValenciaMoralesAngel <21340534@itnogales.edu.mx>
2022-11-30 05:51:45 -0600

    curso de java primer video datos primitivos

---
+++ src/Primitivos.java
@@ -0,0 +1,12 @@
+public class Primitivos {
+    public static void main(String[] args) {
+
+        byte numeroByte = 127;
+        System.out.println("numeroByte = " + numeroByte);// se almacenan en 8 bits
+        System.out.println(" tipo bye corresponde  " + Byte.BYTES);
+        System.out.println(" tipo bye corresponde " + Byte.SIZE);
+        System.out.println(" Maximo en bytes " + Byte.MAX_VALUE);
+        System.out.println(" Minimo en bytes " + Byte.MIN_VALUE);
+
+    }
+}
